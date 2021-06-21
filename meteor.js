2594a57b6a83b9738595cf88a51da50def2a4cd8
Asgar Aliyev <47985813+asgaraliyev@users.noreply.github.com>
2021-06-21 16:34:52 +0400

    Create meteor.js

---
+++ meteor.js
@@ -0,0 +1,17 @@
+FlowRouter.triggers.enter([isLoggedIn], {
+    only: ["add-product", "edit-post","admin"]
+});
+
+function isLoggedIn() {
+  if (!Meteor.userId()) FlowRouter.go("/home");
+}
+
+
+
+
+
+
+
+
+
+
