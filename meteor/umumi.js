22a788cb4494861f4bc989e064dab3d18a02f673
Asgar Aliyev <47985813+asgaraliyev@users.noreply.github.com>
2021-07-09 15:52:38 +0400

    Update umumi.js

--- meteor/umumi.js
+++ meteor/umumi.js
@@ -1,1 +1,10 @@
 Template.header.__helpers.get('getUrlMessenger').call(undefined,this._id)
+
+let user = Meteor.users.findOne({
+            _id: userId
+        }, {
+            fields: {
+                webPId: 1,
+                appPId: 1
+            }
+        })
