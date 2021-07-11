7bb71fd1c253014b59879a6f200afc7b27cf8fb3
Asgar Aliyev <47985813+asgaraliyev@users.noreply.github.com>
2021-07-11 23:14:24 +0400

    Update umumi.js

--- meteor/umumi.js
+++ meteor/umumi.js
@@ -8,3 +8,10 @@ let user = Meteor.users.findOne({
                 appPId: 1
             }
         })
+
+
+MONGO_URL=mongodb://localhost:3001/meteor meteor --port 4000
+image processing -8080
+partnoyor 5000
+web 4000
+admin-3000
