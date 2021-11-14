c623d4b584efb99c57fb5cab2ac8242b0a577d1a
Asgar Aliyev <47985813+asgaraliyev@users.noreply.github.com>
2021-11-14 23:23:04 +0400

    Create react-router-dom-best-routing.jsx

---
+++ react-router-dom-best-routing.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import layouts from "./routes";
+import NotFound from "./pages/NotFound";
+import { Provider } from "react-redux";
+import store from "./redux/index";
+import { Skeleton } from "antd";
+import { timeTravel as timeT } from "../helpers/client";
+timeTravel = timeT;
+Sk = ({ children, loading, ...rest }) => {
+  return (
+    <>
+      {loading === true ? (
+        <Skeleton loading={loading} {...rest}></Skeleton>
+      ) : (
+        children
+      )}
+    </>
+  );
+};
+function Login() {
+  return <h1>Login</h1>;
+}
+function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
+function AuthLayout({ children }) {
+  return <>AuthLayout{children}</>;
+}
+function MainLayout({ children }) {
+  return <>MainLayout{children}</>;
+}
+function ParentLayout({ children }) {
+  return <>ParentLayout {children}</>;
+}
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path={["/login","/login2"]}>
+            <AuthLayout>
+              <Switch>
+                <Route exact path={"/login"} component={Login} />
+                <Route exact path={"/login2"} component={Login} />
+              </Switch>
+            </AuthLayout>
+          </Route>
+
+          <Route exact path={["/dashboard","/dashboard2"]}>
+            <MainLayout>
+              <Switch>
+                <Route exact path={"/dashboard"} component={Dashboard} />
+                <Route exact path={"/dashboard2"} component={Dashboard} />
+              </Switch>
+            </MainLayout>
+          </Route>
+
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </Provider>
+  );
+}
